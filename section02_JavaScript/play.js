0ea2c6ecadea6196e9fc86deecb5d1f3f655e8ac
Clériston Martinelo <cleriston@gmail.com>
2021-02-16 22:55:16 -0300

    12 refreshing the core syntax

---
+++ section02_JavaScript/play.js
@@ -0,0 +1,16 @@
+var name = 'Keko';
+var age = 39;
+var hasHobbies = true;
+
+function summarizeUser(userName, userAge, userHasHobby){
+    return ('Name is '+ 
+            userName +
+            ', age is  '+
+            userAge +
+            ' and the user has hobbies: '+
+            userHasHobby
+            );
+}
+
+
+console.log(summarizeUser(name, age, hasHobbies));
