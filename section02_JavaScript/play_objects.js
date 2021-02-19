30c942cabb8546761a19b53845205abad4a0dcac
Clériston Martinelo <cleriston@gmail.com>
2021-02-19 13:44:52 -0300

    18 Spread and Rest operators

--- section02_JavaScript/play_objects.js
+++ section02_JavaScript/play_objects.js
@@ -10,6 +10,9 @@ const person = {
 console.log(person);
 person.greet();
 
+const copiedPerson = {...person};
+console.log(copiedPerson);
+
 const hobbies = ['Sports', 'Cooking'];
 
 for (let hobby of hobbies){
@@ -17,3 +20,14 @@ for (let hobby of hobbies){
 }
 console.log(hobbies.map(hobby => 'Hobby: ' + hobby + ' is nice'));
 console.log(hobbies);
+
+hobbies.push('Programming');
+console.log(hobbies);
+
+const copiedHobbies = [...hobbies];
+console.log(copiedHobbies);
+
+//rest operators
+const toArray = (...args) =>  args
+
+console.log(toArray(1,2,3,4));
