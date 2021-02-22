ea15c6854dbdba8af9275e3c66d5b67022269f17
Clériston Martinelo <cleriston@gmail.com>
2021-02-22 12:30:57 -0300

    30. Sending Responses

--- section03_basics/app.js
+++ section03_basics/app.js
@@ -4,8 +4,14 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method, req.headers);
-    process.exit();
-
+    // process.exit();
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My text</title></head>')
+    res.write('<body><h1>Hello from my Node.js server</h1></body>');
+    res.write('</html>');
+    res.end();
+    
 });
 
 server.listen(3000);
