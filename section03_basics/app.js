0ed144e23d22e3ab48cab1ed5622f2c52114dd25
Clériston Martinelo <cleriston@gmail.com>
2021-02-22 12:53:48 -0300

    33. Redirecting Requests

--- section03_basics/app.js
+++ section03_basics/app.js
@@ -1,11 +1,12 @@
 //Import Node HTTP Core module 
 const http = require('http');
-
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method, req.headers);
     // process.exit();
     const url = req.url;
+    const method = req.method;
 
     if ( url === '/'){
         res.setHeader('Content-Type', 'text/html');
@@ -13,6 +14,14 @@ const server = http.createServer((req, res) => {
         res.write('<head><title>My text</title></head>')
         res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
         res.write('</html>');
+        return res.end();
+    }
+
+    if ( url === '/message' && method === 'POST'){
+        fs.writeFileSync('/tmp/message.txt', 'DUMMY');
+        // redirect code
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
         return res.end();
     }
     res.setHeader('Content-Type', 'text/html');
