05697912fa8f9441677dcefbad2723b2bd3e029d
Clériston Martinelo <cleriston@gmail.com>
2021-02-28 08:55:18 -0300

    63. Handling Different Routes

--- sectio05_01-express/app.js
+++ sectio05_01-express/app.js
@@ -11,10 +11,23 @@ app.use( (req, res, next) => {
     next(); 
 })
 
-app.use( (req, res, next) => {
+app.use('/', (req, res, next) => {
+    console.log("This always runs!");
+    next();
+})
+
+app.use('/add-product', (req, res, next) => {
+    console.log("In the 'add-product' middleware");
+    res.send("<h1>The 'add-product' page</h1>")
+})
+
+app.use('/', (req, res, next) => {
     console.log("In the another middleware");
     res.send("<h1>Hello from Express!</h1>")
 })
+
+
+
 
 // const server = http.createServer(app);
 
