d43f53d80d50cad4549e58ebbdc94383dfdc4be4
Clériston Martinelo <cleriston@gmail.com>
2021-02-28 15:08:06 -0300

    72. Using a Helper Function for Navigation

--- section05_01-express/routes/admin.js
+++ section05_01-express/routes/admin.js
@@ -2,11 +2,14 @@ const express = require('express');
 
 const path = require('path');
 
+const rootDir = require('../util/path');
+
 const router = express.Router();
 
 router.use('/add-product', (req, res, next) => {
     console.log("In the 'add-product' middleware");
-    res.sendFile(path.join(__dirname, '../', 'views', 'product.html'));
+    // res.sendFile(path.join(__dirname, '../', 'views', 'product.html'));
+    res.sendFile(path.join(rootDir, 'views', 'product.html'));
 })
 
 router.post('/product', (req, res, next) => {
--- section05_01-express/routes/shop.js
+++ section05_01-express/routes/shop.js
@@ -1,6 +1,9 @@
 const express = require('express');
 
 const path = require('path')
+
+const rootDir = require('../util/path');
+
 const router = express.Router();
 
 router.use('/', (req, res, next) => {
@@ -10,7 +13,8 @@ router.use('/', (req, res, next) => {
 
 router.get('/', (req, res, next) => {
     console.log("In the another middleware");
-    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
+    //res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
+    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 })
 
 module.exports = router;
