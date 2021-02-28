e1482bf5ba754d95ff217e02732e81cf52e01966
Clériston Martinelo <cleriston@gmail.com>
2021-02-28 11:07:13 -0300

    66. Using Express Router

--- section05_01-express/app.js
+++ section05_01-express/app.js
@@ -1,34 +1,16 @@
-const http = require('http');
-
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 //This middleware parse our chunks of body in a body
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/', (req, res, next) => {
-    console.log("This always runs!");
-    next();
-})
-
-app.use('/add-product', (req, res, next) => {
-    console.log("In the 'add-product' middleware");
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Submit</button></form>');
-})
-
-app.post('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect("/");
-})
-
-app.use('/', (req, res, next) => {
-    console.log("In the another middleware");
-    res.send('<h1>Hello from Express!</h1><p><a href="add-product">Add product</a></p>');
-})
-
-
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 
 // const server = http.createServer(app);
---
+++ section05_01-express/routes/shop.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.use('/', (req, res, next) => {
+    console.log("This always runs!");
+    next();
+})
+
+router.use('/', (req, res, next) => {
+    console.log("In the another middleware");
+    res.send('<h1>Hello from Express!</h1><p><a href="add-product">Add product</a></p>');
+})
+
+module.exports = router;
