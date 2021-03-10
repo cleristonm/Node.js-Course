6fefa0cd9431bec71f3bfafae60f24a240239a8c
Clériston Martinelo <cleriston@gmail.com>
2021-03-10 15:53:01 -0300

    87. Working with Handlebars

--- udemy course/section06_01-express_Dynamic_Content_Templating_Engine/app.js
+++ udemy course/section06_01-express_Dynamic_Content_Templating_Engine/app.js
@@ -2,11 +2,13 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars');
 
 const app = express();
 
-app.set('view engine', 'pug');
-
+app.engine('hbs', expressHbs());
+app.set('view engine', 'hbs');
+//app.set('view engine', 'pug');
 //this is the default value
 app.set('views', 'views');
 
