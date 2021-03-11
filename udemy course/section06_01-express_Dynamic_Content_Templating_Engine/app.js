9a0992810aeda560e9f8f4366c5754d5a8023069
Clériston Martinelo <cleriston@gmail.com>
2021-03-11 11:29:43 -0300

    89. Adding the Layout to Handlebars

--- udemy course/section06_01-express_Dynamic_Content_Templating_Engine/app.js
+++ udemy course/section06_01-express_Dynamic_Content_Templating_Engine/app.js
@@ -6,7 +6,11 @@ const expressHbs = require('express-handlebars');
 
 const app = express();
 
-app.engine('hbs', expressHbs());
+app.engine('hbs', expressHbs({ 
+    layoutsDir: 'views/layouts/', 
+    defaultLayout: 'main-layout.hbs', 
+    extname: 'hbs'}));
+    
 //hbs is the file's extesion 
 app.set('view engine', 'hbs');
 //app.set('view engine', 'pug');
