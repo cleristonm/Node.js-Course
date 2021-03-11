579e5e82a9ff5ded1874c9f527f1d3414b4e069e
Clériston Martinelo <cleriston@gmail.com>
2021-03-11 18:03:08 -0300

    101. Fetching Data from Files Via the Model pt1

--- udemy course/section07_MVC/models/product.js
+++ udemy course/section07_MVC/models/product.js
@@ -7,18 +7,23 @@ module.exports = class Product {
         this.title = t;
     }
 
-    save(){
+    save() {
         const p = path.join(
-            path.dirname(process.mainModule.filename), 
-            'data', 
-            'products.json');
-        fs.readFile(p, (err, fileContent) => {            
-            if (err){
-                return [];                
-            }
-            return JSON.parse(fileContent);            
-        })
-    }
+          path.dirname(process.mainModule.filename),
+          'data',
+          'products.json'
+        );
+        fs.readFile(p, (err, fileContent) => {
+          let products = [];
+          if (!err) {
+            products = JSON.parse(fileContent);
+          }
+          products.push(this);
+          fs.writeFile(p, JSON.stringify(products), err => {
+            console.log(err);
+          });
+        });
+      }
 
     static fetchAll(cb){
         const p = path.join(
